c0d1e4e9209fcdb56f90ad667012072d76f89347
Tom Dörr <tom.doerr@tum.de>
2021-10-13 23:24:52 +0200

    Add files

---
+++ app.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+'''
+This script displays information about the applicants and let's the
+user score an applicant.
+'''
+
+import streamlit as st
+st.set_page_config(page_title="Simple Auth", layout="wide")
+
+import streamlit_debug
+streamlit_debug.set(flag=False, wait_for_client=True, host='localhost', port=8765)
+
+import env
+env.verify()
+
+from authlib.auth import auth, authenticated, requires_auth
+from authlib.common import trace_activity
+from sqlalchemy import select
+import sys
+
+user = auth(sidebar=True, show_msgs=True)
+
+st.title('Test App')
+if authenticated():
+    st.success(f'`{user}` is authenticated')
+else:
+    st.warning(f'Not authenticated')
+    # Stop running.
+    st.stop()
+    sys.exit()
+
+
+# Let the user input the ID of the applicant that is supposed to be displayed.
+st.sidebar.title('User Selection')
+applicant_id = st.sidebar.text_input('Applicant ID', value='4')
+
+# Show the user id.
+st.title(f'User #{applicant_id}')
+
+
+
+
+db_url = st.secrets['db_url']
+
+
+# Connect
+import sqlalchemy
+engine = sqlalchemy.create_engine(db_url)
+engine.connect()
+
+from sqlalchemy import MetaData
+meta = MetaData()
+meta.reflect(bind=engine)
+
+# Show all available fields in the table.
+from sqlalchemy import inspect
+inspector = inspect(engine)
+
+columns = inspector.get_columns('api_appform')
+
+# The above items as a list.
+columns = ['firstname',
+           'lastname',
+           'email',
+           'phone',
+           'languages',
+           'age',
+           'nationality',
+           'gender',
+           'resume',
+           'university',
+           'degree',
+           'major',
+           'semester',
+           'hours',
+           'city',
+           'q1',
+           'q2',
+           'q3',
+           'q4',
+           'q5',
+           'q6',
+           'q7',
+           'q8']
+
+# Show the value for all columns.
+for item in columns:
+    table = meta.tables['api_appform']
+    query = select([table.c.id, table.c[item]]).where(table.c.id == int(applicant_id))
+    with engine.connect() as conn:
+        result = conn.execute(query).fetchone()
+    item_name_capitalized = item.capitalize()
+    st.write(f'{item_name_capitalized}: {result[1]}')
+
+
+
+
+# Let the user select a score.
+score = st.slider('Score', 1, 10, 5)
